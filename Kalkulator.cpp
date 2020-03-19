68ff7317b2ea2dca92a98a748064b0355853a88b
Dejvid238 <dawidkrekora@gmail.com>
2020-03-19 02:29:21 +0100

    Prosty Kalkulator

--- Kalkulator.cpp
+++ Kalkulator.cpp
@@ -4,6 +4,50 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+enum opcja {Dodaj=1,Odejmij,Mnoz,Dziel,Poteguj};
+
+int liczba;
+void Menu (){
+  for (int i=1;i>0;){
+cout<<"Wybierz co chcesz zrobic: "<<endl;
+cout<<"1 - Dodawanie "<<endl;
+cout<<"2 - Odejmowanie "<<endl;
+cout<<"3 - Mnozenie "<<endl;
+cout<<"4 - Dzielenie "<<endl;
+cout<<"5 - Potegowanie "<<endl;
+
+cin>>liczba;
+
+ switch (liczba){
+   case Dodaj:
+   liczba=Dodaj;
+   i=0;
+   break;
+   case Odejmij:
+   liczba=Odejmij;
+   i=0;
+   break;
+   case Mnoz:
+   liczba=Mnoz;
+   i=0;
+   break;
+   case Dziel:
+   liczba=Dziel;
+   i=0;
+   break;
+   case Poteguj:
+   liczba=Poteguj;
+   i=0;
+   break;
+   default:
+   cout<<"Wybrano zly numer, sprobuj ponownie..."<<endl;
+   i++;
+   break;
+   }
+  }
+ }
+
+
 void Dodawanie(long long &L1,long long &L2){
 long long wynik=L1+L2;
 cout<<"Wynik dodawania to: "<<wynik<<endl;
@@ -37,6 +81,7 @@ void Potegowanie(long long &L1,long long &L2){
 }
 
 int main(){
+
 long long zmienna1,zmienna2;
 cout<<"Podaj pierwsza liczbe: ";
 cin>>zmienna1;
@@ -44,11 +89,14 @@ cout<<endl;
 cout<<"Podaj druga liczbe: ";
 cin>>zmienna2;
 cout<<endl;
-Dodawanie(zmienna1,zmienna2);
-Odejmowanie(zmienna1,zmienna2);
-Mnozenie(zmienna1,zmienna2);
-Dzielenie(zmienna1,zmienna2);
-Potegowanie(zmienna1,zmienna2);
+
+Menu();
+if (liczba==1) Dodawanie(zmienna1,zmienna2);
+else if (liczba==2) Odejmowanie(zmienna1,zmienna2);
+else if (liczba==3) Mnozenie(zmienna1,zmienna2);
+else if (liczba==4) Dzielenie(zmienna1,zmienna2);
+else if (liczba==5) Potegowanie(zmienna1,zmienna2);
+
 getchar();
 return 0;
 }
