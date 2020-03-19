f842e6c66b125bc98bd500b62b68737044b428a3
Dejvid238 <62191249+Dejvid238@users.noreply.github.com>
2020-03-19 02:45:59 +0100

    Update Kalkulator.cpp

--- Kalkulator.cpp
+++ Kalkulator.cpp
@@ -8,42 +8,43 @@ using std::endl;
 enum opcja {Dodaj=1,Odejmij,Mnoz,Dziel,Poteguj};
 
 int liczba;
+
 void Menu (){
   for (int i=1;i>0;){
-cout<<"Wybierz co chcesz zrobic: "<<endl;
-cout<<"1 - Dodawanie "<<endl;
-cout<<"2 - Odejmowanie "<<endl;
-cout<<"3 - Mnozenie "<<endl;
-cout<<"4 - Dzielenie "<<endl;
-cout<<"5 - Potegowanie "<<endl;
+     cout<<"Wybierz co chcesz zrobic: "<<endl;
+     cout<<"1 - Dodawanie "<<endl;
+     cout<<"2 - Odejmowanie "<<endl;
+     cout<<"3 - Mnozenie "<<endl;
+     cout<<"4 - Dzielenie "<<endl;
+     cout<<"5 - Potegowanie "<<endl;
 
 cin>>liczba;
 
- switch (liczba){
+switch (liczba){
    case Dodaj:
-   liczba=Dodaj;
-   i=0;
-   break;
+     liczba=Dodaj;
+     i=0;
+     break;
    case Odejmij:
-   liczba=Odejmij;
-   i=0;
-   break;
+     liczba=Odejmij;
+     i=0;
+     break;
    case Mnoz:
-   liczba=Mnoz;
-   i=0;
-   break;
+     liczba=Mnoz;
+     i=0;
+     break;
    case Dziel:
-   liczba=Dziel;
-   i=0;
-   break;
+     liczba=Dziel;
+     i=0;
+     break;
    case Poteguj:
-   liczba=Poteguj;
-   i=0;
-   break;
+     liczba=Poteguj;
+     i=0;
+     break;
    default:
-   system("clear");
-   cout<<"Wybrano zly numer, sprobuj ponownie..."<<endl<<endl;
-   i++;
+     system("clear");
+     cout<<"Wybrano zly numer, sprobuj ponownie..."<<endl<<endl;
+     i++;
    break;
    }
   }
@@ -51,40 +52,39 @@ cin>>liczba;
 
 
 void Dodawanie(long long &L1,long long &L2){
-long long wynik=L1+L2;
-cout<<"Wynik dodawania to: "<<wynik<<endl;
-}
+     long long wynik=L1+L2;
+     cout<<"Wynik dodawania to: "<<wynik<<endl;}
+
 void Odejmowanie(long long &L1,long long &L2){
-long long wynik=L1-L2;
-cout<<"Wynik odejmowania to: "<<wynik<<endl;
-}
+     long long wynik=L1-L2;
+     cout<<"Wynik odejmowania to: "<<wynik<<endl;}
+
 void Mnozenie(long long &L1,long long &L2){
-long long wynik=L1*L2;
-cout<<"Wynik mnozenia to: "<<wynik<<endl;
-}
+     long long wynik=L1*L2;
+     cout<<"Wynik mnozenia to: "<<wynik<<endl;}
+
 void Dzielenie(long long &L1,long long &L2){
-  if (L2==0) {
-  cout<<"Nie mozna dzielic przez 0!"<<endl;}
-  else{
-  long long wynik=L1/L2;
-  cout<<"Wynik dzielenia to: "<<wynik<<endl;}
-  }
+     if (L2==0) {
+        cout<<"Nie mozna dzielic przez 0!"<<endl;}
+     else{
+        long long wynik=L1/L2;
+        cout<<"Wynik dzielenia to: "<<wynik<<endl;}}
+
 void Potegowanie(long long &L1,long long &L2){
-    long long baza=L1;
-    if (L2==0){
-      baza=1;}
-    else if (L2==1){
-      baza=L1;}
-    else {
-      for (int i=2;i<=L2;i++){
-         baza*=L1;}
-         }
-    cout<<"Wynik potegowania to: "<<baza<<endl;
-}
+     long long baza=L1;
+     if (L2==0){
+        baza=1;}
+     else if (L2==1){
+        baza=L1;}
+     else {
+        for (int i=2;i<=L2;i++){
+           baza*=L1;}}
+     cout<<"Wynik potegowania to: "<<baza<<endl;}
 
 int main(){
 
 long long zmienna1,zmienna2;
+
 cout<<"Podaj pierwsza liczbe: ";
 cin>>zmienna1;
 cout<<endl;
