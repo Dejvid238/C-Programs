f16acd552cfd2931c2a6bb90cfd2380d43fc82d2
Dejvid238 <dawidkrekora@gmail.com>
2020-04-01 00:24:48 +0200

    Plik glowny klasy Uzytkownik

--- Pierwsze_klasy/main.cpp
+++ Pierwsze_klasy/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include "uzytkownik.h"
 
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+    Uzytkownik user[5];
+
+
+    cout<<user[0].getID()<<endl;
+    cout<<user[1].getID()<<endl;
+
+    cout<<Uzytkownik::getCounter()<<endl;   //wypisuje ostatnia wartosc ktora zostaje przypisana dla miennej counter
+
+    getchar();
+    return EXIT_SUCCESS;
+}
