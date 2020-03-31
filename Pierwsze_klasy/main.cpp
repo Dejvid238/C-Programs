034e6cd33a60d4e5fc9c3dc86d832fb38a663966
Dejvid238 <dawidkrekora@gmail.com>
2020-04-01 01:50:19 +0200

    Pierwsze-

--- Pierwsze_klasy/main.cpp
+++ Pierwsze_klasy/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
     cout<<user[0].getID()<<endl;
     cout<<user[1].getID()<<endl;
 
-    cout<<Uzytkownik::getCounter()<<endl;   //wypisuje ostatnia wartosc ktora zostaje przypisana dla miennej counter
+    cout<<Uzytkownik::getCounter()<<endl;   //wypisuje ostatnia wartosc ktora zostaje przypisana dla zmiennej counter
 
     getchar();
     return EXIT_SUCCESS;
